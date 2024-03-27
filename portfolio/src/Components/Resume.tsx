0bffd191ba994b7c77116145642abdaf7149f95c
Tamil <tamilselvan.k@arkinfotec.com>
2024-03-27 18:16:06 +0530

    added animation for resume

--- portfolio/src/Components/Resume.tsx
+++ portfolio/src/Components/Resume.tsx
@@ -3,12 +3,11 @@ import { experienceData } from "../datas/ExperienceData";
 import { motion } from "framer-motion";
 import { educationQualification } from "../datas/EducationData";
 
-
-const StyledBox = styled(Box)({
+const StyledBox = styled(motion.div)({
   padding:20,
   marginBottom: 2,
   backgroundColor: "#003366",
-  transition: "background-color 0.5s ease-in-out",
+  transition: "background-color 0.5s ease-in-out, transform 0.5s ease-in-out",
   textAlign: "center",
   width: "90%",
   maxWidth: "600px",
@@ -16,6 +15,7 @@ const StyledBox = styled(Box)({
   "&:hover": {
     backgroundColor: "#0080ff",
     backgroundImage: "linear-gradient(to right, #00008B, #00BFFF)",
+    transform: "scale(1.1)", 
   },
 });
 
@@ -35,52 +35,58 @@ const Resume = () => {
 
   const renderExperience = () => {
     return Object.keys(experienceData.experience).map((key, index) => (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
+      <StyledBox
         key={index}
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, delay: 0.1 * index }}
       >
-        <StyledBox>
-          <StyledTypography variant="h6">{experienceData.experience[key as keyof typeof experienceData.experience].name}</StyledTypography>
+         <StyledTypography variant="h6">{experienceData.experience[key as keyof typeof experienceData.experience].name}</StyledTypography>
           <StyledTypography variant="subtitle1">{experienceData.experience[key as keyof typeof experienceData.experience].year}</StyledTypography>
           <StyledTypography variant="body2">{experienceData.experience[key as keyof typeof experienceData.experience].company}</StyledTypography>
           <StyledTypography variant="body2">{experienceData.experience[key as keyof typeof experienceData.experience].city}</StyledTypography>
-        </StyledBox>
-      </motion.div>
+      </StyledBox>
     ));
   };
   
-
   const renderEducationQualifications = () => {
     return Object.keys(educationQualification.course).map((key, index) => (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
+      <StyledBox
         key={index}
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, delay: 0.1 * index }}
       >
-        <StyledBox>
-          <StyledTypography variant="h6">{educationQualification.course[key as keyof typeof educationQualification.course].name}</StyledTypography>
+       <StyledTypography variant="h6">{educationQualification.course[key as keyof typeof educationQualification.course].name}</StyledTypography>
           <StyledTypography variant="subtitle1">{educationQualification.course[key as keyof typeof educationQualification.course].year}</StyledTypography>
           <StyledTypography variant="body2">{educationQualification.course[key as keyof typeof educationQualification.course].institution}</StyledTypography>
           <StyledTypography variant="body2">{educationQualification.course[key as keyof typeof educationQualification.course].city}</StyledTypography>
-        </StyledBox>
-      </motion.div>
+      </StyledBox>
     ));
   };
-  
 
   return (
     <Box display="flex" flexDirection={isMobile ? 'column' : 'row'} justifyContent="center" alignItems="center" minHeight="100vh" padding={3} sx={{ backgroundImage: "linear-gradient(to right, #00008B, #00BFFF)" }}>
       <Stack flex="1" spacing={2} padding={3}>
-        <StyledTypography variant="h3" >{titles.education}</StyledTypography>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <StyledTypography variant="h3">{titles.education}</StyledTypography>
+        </motion.div>
         {renderEducationQualifications()}
       </Stack>
       <Stack flex="1" spacing={2}>
-        <StyledTypography variant="h3">{titles.experience}</StyledTypography>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <StyledTypography variant="h3">{titles.experience}</StyledTypography>
+        </motion.div>
         {renderExperience()}
       </Stack>
     </Box>
